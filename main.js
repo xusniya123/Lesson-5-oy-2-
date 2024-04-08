20e1abe227e2ec78654ecbafcf21349ad8e41519
xusniya123 <xusniya12345678@gmail.com>
2024-04-08 16:55:21 +0500

    Lesson 5 moth (2)

---
+++ main.js
@@ -0,0 +1,21 @@
+// let person = {
+//     name: "Xusniya",
+//     age: 14,
+//   play: function volebol() {
+//     console.log("Gooll");
+//   }
+// }
+
+// console.log(person);
+let person = {
+ name: prompt("Ismingizni kiriting"),
+ id: prompt("space id kiriting"),
+ possword: prompt("space possword kiriting"),
+}
+console.log(person);
+
+
+
+
+
+
